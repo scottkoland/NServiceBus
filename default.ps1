8f7f229aa4af4d9a92d22ebfcc7b8c6ff87f44f0
Johannes Gustafsson <johannesgu@gmail.com>
2012-05-18 22:55:16 +0200

    ILMerge log4net into Core and Host because Topshelf and Rhino licensing needs it.

--- default.ps1
+++ default.ps1
@@ -164,7 +164,7 @@ task CompileCore -depends InitEnvironment -description "Builds NServiceBus.Core.
 	
 	<#It's Possible to copy the NServiceBus.Core.dll to Core-Only but not done gain time on development build #>
 			
-	$assemblies += dir $buildBase\nservicebus.core\antlr3*.dll
+	$assemblies += dir $buildBase\nservicebus.core\log4net.dll
 	#$assemblies += dir $buildBase\nservicebus.core\common.logging.dll
 	#$assemblies += dir $buildBase\nservicebus.core\common.logging.log4net.dll
 	$assemblies += dir $buildBase\nservicebus.core\Interop.MSMQ.dll
@@ -326,7 +326,7 @@ task CompileHosts  -depends InitEnvironment -description "Builds NServiceBus.Hos
 	}
 	
 	$assemblies = @("$buildBase\hosting\NServiceBus.Hosting.Windows.exe", "$buildBase\hosting\NServiceBus.Hosting.dll",
-		"$buildBase\hosting\Microsoft.Practices.ServiceLocation.dll", "$buildBase\hosting\Magnum.dll", "$buildBase\hosting\Topshelf.dll")
+		"$buildBase\hosting\Microsoft.Practices.ServiceLocation.dll", "$buildBase\hosting\Magnum.dll", "$buildBase\hosting\Topshelf.dll", "$buildBase\hosting\log4net.dll")
 	
 	Ilmerge $ilMergeKey $outDir\host\ "NServiceBus.Host" $assemblies "" "exe"  $script:ilmergeTargetFramework "$buildBase\NServiceBusHostMergeLog.txt"  $ilMergeExclude
 	
@@ -346,7 +346,7 @@ task CompileHosts32  -depends InitEnvironment -description "Builds NServiceBus.H
 	
 	
 	$assemblies = @("$buildBase\hosting32\NServiceBus.Hosting.Windows.exe", "$buildBase\hosting32\NServiceBus.Hosting.dll",
-		"$buildBase\hosting32\Microsoft.Practices.ServiceLocation.dll", "$buildBase\hosting32\Magnum.dll", "$buildBase\hosting32\Topshelf.dll")
+		"$buildBase\hosting32\Microsoft.Practices.ServiceLocation.dll", "$buildBase\hosting32\Magnum.dll", "$buildBase\hosting32\Topshelf.dll", "$buildBase\hosting32\log4net.dll")
 	
 	Ilmerge $ilMergeKey $outDir\host\ "NServiceBus.Host32" $assemblies "" "exe"  $script:ilmergeTargetFramework "$buildBase\NServiceBusHostMerge32Log.txt"  $ilMergeExclude
 	
